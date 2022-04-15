684d6b9b75a1a199acb737bcd522595bce33c4a0
PLATnya <vadim.plataniy@gmail.com>
2022-04-15 20:09:36 +0300

    Add health component setup

---
+++ Source/FirstPersonPlayer/Components/FPHealthComponent.cpp
@@ -0,0 +1,52 @@
+﻿// 
+
+
+#include "FPHealthComponent.h"
+#include "GameFramework/Character.h"
+
+
+UFPHealthComponent::UFPHealthComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UFPHealthComponent::AddHealthPoints(float _HealthPoints)
+{
+	const float OldHealth = HealthPoints;
+	HealthPoints += _HealthPoints;
+	
+	OnHealthChanging.Broadcast(OldHealth, HealthPoints);
+}
+
+void UFPHealthComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	auto* pCharacter = Cast<ACharacter>(GetOwner());
+	if (IsValid(pCharacter))
+	{
+		if (!pCharacter->OnTakeAnyDamage.IsAlreadyBound(this, &UFPHealthComponent::OnTakeAnyDamage))
+		{
+			pCharacter->OnTakeAnyDamage.AddDynamic(this, &UFPHealthComponent::OnTakeAnyDamage);
+		}
+	}
+}
+
+
+void UFPHealthComponent::OnTakeAnyDamage(AActor* DamagedActor, float Damage,
+	const UDamageType* DamageType, AController* InstigatedBy, AActor* DamageCauser)
+{
+	if (!bIsAlive)
+	{
+		return;
+	}
+
+	const float OldHealth = HealthPoints;
+	HealthPoints = FMath::Clamp(HealthPoints - Damage, 0.0f, MaxHealthPoints);
+
+	OnHealthChanging.Broadcast(OldHealth, HealthPoints);
+	if (HealthPoints <= KINDA_SMALL_NUMBER)
+	{
+		bIsAlive = false;
+		OnDeath.Broadcast();
+	}	
+}
---
+++ Source/FirstPersonPlayer/Components/FPHealthComponent.h
@@ -0,0 +1,37 @@
+﻿// 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "FPHealthComponent.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FMOnDeath);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FMOnHealthChanging, float, OldHealth, float, NewHealth);
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class FIRSTPERSONPLAYER_API UFPHealthComponent : public UActorComponent
+{
+	GENERATED_BODY()
+public:
+	FMOnDeath OnDeath;
+	FMOnHealthChanging OnHealthChanging;
+	
+	UFPHealthComponent();
+	UFUNCTION(BlueprintCallable)
+	void AddHealthPoints(float _HealthPoints);
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(BlueprintReadOnly)
+	float HealthPoints = 100.0f;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	float MaxHealthPoints = 100.0f;
+private:
+
+	bool bIsAlive = true;
+
+	UFUNCTION()
+	void OnTakeAnyDamage(AActor* DamagedActor, float Damage, const class UDamageType* DamageType, class AController* InstigatedBy, AActor* DamageCauser);
+	
+};

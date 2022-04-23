b50383cc696a1278cc2a85f821c70d368e409e7e
PLATnya <vadim.plataniy@gmail.com>
2022-04-23 17:12:35 +0300

    Setup climbing components

---
+++ Source/FirstPersonPlayer/Components/FPClimbingComponent.cpp
@@ -0,0 +1,25 @@
+﻿//
+
+
+#include "FPClimbingComponent.h"
+
+
+// Sets default values for this component's properties
+UFPClimbingComponent::UFPClimbingComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UFPClimbingComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+
+}
---
+++ Source/FirstPersonPlayer/Components/FPClimbingComponent.h
@@ -0,0 +1,28 @@
+﻿//
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "FPClimbingComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class FIRSTPERSONPLAYER_API UFPClimbingComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UFPClimbingComponent();
+
+	UPrimitiveComponent* GetGrabbedPrimitiveComp() const { return pPrimitiveGrabbedObstacle; }
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+private:
+
+	UPROPERTY()
+	UPrimitiveComponent* pPrimitiveGrabbedObstacle = nullptr;
+};

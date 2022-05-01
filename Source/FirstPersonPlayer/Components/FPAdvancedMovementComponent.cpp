a16f2f4e288a4ec3ea8c6fe9225336d8c58f6f1b
PLATnya <vadim.plataniy@gmail.com>
2022-05-01 18:16:51 +0300

    Add custom movement component for movement extensions; add simple sprinting

---
+++ Source/FirstPersonPlayer/Components/FPAdvancedMovementComponent.cpp
@@ -0,0 +1,67 @@
+﻿//
+
+
+#include "FPAdvancedMovementComponent.h"
+
+#include "FirstPersonPlayer/FPCharacter.h"
+
+UFPAdvancedMovementComponent::UFPAdvancedMovementComponent(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	PrimaryComponentTick.TickGroup = TG_PrePhysics;
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+
+void UFPAdvancedMovementComponent::StartSprinting()
+{
+	if(bIsSprinting)
+	{
+		return;
+	}
+
+	UCharacterMovementComponent* CharacterMovement = GetCharacterMovement();
+	if(!ensureMsgf(IsValid(CharacterMovement), TEXT("Character Movement component is missing to start sprinting")))
+	{
+		return;
+	}
+
+	BuffMaxWalkSpeed = CharacterMovement->MaxWalkSpeed;
+	CharacterMovement->MaxWalkSpeed = RunSpeed;
+
+	bIsSprinting = true;
+}
+
+void UFPAdvancedMovementComponent::StopSprinting()
+{
+	if(!bIsSprinting)
+	{
+		return;
+	}
+
+	UCharacterMovementComponent* CharacterMovement = GetCharacterMovement();
+	if(!ensureMsgf(IsValid(CharacterMovement), TEXT("Character Movement component is missing to stop sprinting")))
+	{
+		return;
+	}
+
+	CharacterMovement->MaxWalkSpeed = BuffMaxWalkSpeed;
+
+	bIsSprinting = false;
+}
+
+AFPCharacter* UFPAdvancedMovementComponent::GetCharacterOwner() const
+{
+	return Cast<AFPCharacter>(GetOwner());
+}
+
+UCharacterMovementComponent* UFPAdvancedMovementComponent::GetCharacterMovement() const
+{
+	AFPCharacter* OwnerCharacter = GetCharacterOwner();
+	if(!IsValid(OwnerCharacter))
+	{
+		return nullptr;
+	}
+
+	return OwnerCharacter->GetCharacterMovement();
+}
---
+++ Source/FirstPersonPlayer/Components/FPAdvancedMovementComponent.h
@@ -0,0 +1,46 @@
+﻿// 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "FPAdvancedMovementComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class FIRSTPERSONPLAYER_API UFPAdvancedMovementComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+
+	UFPAdvancedMovementComponent(const FObjectInitializer& ObjectInitializer);
+
+	void StartSprinting();
+
+	void StopSprinting();
+
+protected:
+
+
+private:
+
+	UFUNCTION()
+	class AFPCharacter* GetCharacterOwner() const;
+
+	UFUNCTION()
+	class UCharacterMovementComponent* GetCharacterMovement() const;
+
+	UPROPERTY(Transient)
+	float BuffMaxWalkSpeed = 0.0f;
+
+
+	UPROPERTY(EditAnywhere, Category="Running")
+	float RunSpeed = 600.0f;
+
+	UPROPERTY(VisibleAnywhere, Category="Running")
+	bool bIsSprinting = false;
+
+
+};
